4a9b0857c179fb7b613b1927c39b9bb23fde3dfe
ivan <oraionstarr@gmail.com>
2023-11-10 02:45:01 +0800

    final app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: const Text('Dice Dice Baby'),
-          backgroundColor: Colors.red,
-        ),
-        body: const DicePage(),
-      ),
-    ),
-  );
+  return runApp(const MaterialApp(home: DicePage()));
 }
 
 class DicePage extends StatefulWidget {
@@ -27,6 +17,7 @@ class _DicePageState extends State<DicePage> {
   int left = 2;
   int right = 2;
   int counter = 0;
+
   void rand() {
     setState(() {
       left = 1 + Random().nextInt(6);
@@ -35,41 +26,133 @@ class _DicePageState extends State<DicePage> {
     });
   }
 
+  void reset() {
+    setState(() {
+      counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextButton(
-                      onPressed: () {
-                        rand();
-                        print('left image is clicked!');
-                      },
-                      child: Image.asset('images/dice$left.png'),
-                    )),
-              ),
-              Expanded(
-                child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextButton(
-                      onPressed: () {
-                        rand();
-                        print('right image is clicked!');
-                      },
-                      child: Image.asset('images/dice$right.png'),
-                    )),
-              ),
-            ],
+        child: Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/dicePFP.gif'), fit: BoxFit.cover),
           ),
-          Text('Total Dice Roll:$counter'),
-        ],
-      ),
-    );
+        ),
+        DefaultTextStyle(
+          style: const TextStyle(
+            fontSize: 60.0,
+            fontWeight: FontWeight.w900,
+            fontFamily: 'Edu',
+            color: Color.fromARGB(255, 35, 34, 34),
+          ),
+          child: AnimatedTextKit(
+            animatedTexts: [
+              RotateAnimatedText('Dice-'),
+              RotateAnimatedText('Dice--'),
+              RotateAnimatedText('Baby---'),
+            ],
+            repeatForever: true,
+            pause: const Duration(milliseconds: 3000),
+          ),
+        ),
+        Center(
+            child: Container(
+          width: 380,
+          height: 320,
+          decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 255, 179, 193),
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+        )),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextButton(
+                        onPressed: () {
+                          rand();
+                        },
+                        child: Image.asset('images/dice$left.png'),
+                      )),
+                ),
+                Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextButton(
+                        onPressed: () {
+                          rand();
+                        },
+                        child: Image.asset('images/dice$right.png'),
+                      )),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15.0),
+              child: Text(
+                'Total Dice Roll:$counter',
+                style: const TextStyle(
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton.tonal(
+                  onPressed: () {
+                    reset();
+                  },
+                  style: FilledButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 89, 13, 35),
+                    minimumSize: const Size(150, 50),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                  child: const Text(
+                    'Reset',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20.0),
+                FilledButton.tonal(
+                  onPressed: () {
+                    rand();
+                  },
+                  style: FilledButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 89, 13, 35),
+                    minimumSize: const Size(150, 50),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                  child: const Text(
+                    'Roll',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
